cbdc02d97e83240f2e5f0d024c94b79633ce7e99
Aleksashka301 <aleksashka301@gmail.com>
2025-01-16 19:42:16 +0500

    Code optimization

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,17 +1,16 @@
+from helper_functions import saving_image
 import argparse
 import requests
 import os
 
 
-def fetch_spacex_last_launch(images_id):
-    folder = f'photos from space/spacex'
-    os.makedirs(folder, exist_ok=True)
-
+def get_spacex_images(images_id):
     if images_id:
         url = f'https://api.spacexdata.com/v5/launches/{images_id}'
         response = requests.get(url)
         response.raise_for_status()
-        images = response.json()['links']['flickr']['original']
+
+        return response.json()['links']['flickr']['original']
     else:
         url = 'https://api.spacexdata.com/v5/launches'
         response = requests.get(url)
@@ -20,18 +19,16 @@ def fetch_spacex_last_launch(images_id):
 
         for launche in reversed(launches):
             if launche['links']['flickr']['original']:
-                images = launche['links']['flickr']['original']
-                break
+                return launche['links']['flickr']['original']
+
+
+def creation_spacex_images(images):
+    folder = f'photos from space/spacex'
+    os.makedirs(folder, exist_ok=True)
 
     for image_number, url_image in enumerate(images):
-        response = requests.get(url_image)
-        response.raise_for_status()
-
         picture = f'spacex{image_number}.jpg'
-        image_path = os.path.join(folder, picture)
-
-        with open(image_path, 'wb') as file:
-            file.write(response.content)
+        saving_image(folder, picture, url_image)
 
 
 if __name__ == '__main__':
@@ -39,5 +36,8 @@ if __name__ == '__main__':
     parser.add_argument('images_id', nargs='?', default='')
     args = parser.parse_args()
     user_images_id = args.images_id
+    images = get_spacex_images(user_images_id)
 
-    fetch_spacex_last_launch(user_images_id)
+    creation_spacex_images(images)
+
+
