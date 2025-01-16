c7c716d65478e841d66cece5c223692b1f82e84e
Aleksashka301 <aleksashka301@gmail.com>
2025-01-16 19:44:37 +0500

    Function name and adding new functions

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,7 @@
 from urllib.parse import urlparse
+import telegram
+import requests
+import time
 import os
 
 
@@ -9,6 +12,25 @@ def get_file_type(url):
     return file_type
 
 
-def time_interval(timing):
+def calculation_in_seconds(timing):
     seconds_in_minute = timing * 60
     return seconds_in_minute
+
+
+def sending_post(folder, image, bot, channel_id):
+    path_image_post = os.path.join(folder, image)
+    try:
+        with open(path_image_post, 'rb') as picture_for_message:
+            bot.send_document(chat_id=channel_id, document=picture_for_message)
+    except telegram.error.NetworkError:
+        print('Соединение с интернетом разорвано, повторная отправка...')
+        time.sleep(10)
+
+
+def saving_image(folder, image, url, params={}):
+    image_path = os.path.join(folder, image)
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+
+    with open(image_path, 'wb') as file:
+        file.write(response.content)
