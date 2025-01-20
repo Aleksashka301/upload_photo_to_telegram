d02e0a8972c8e66958333053b0a6966fb63d93d2
Aleksashka301 <aleksashka301@gmail.com>
2025-01-20 19:08:12 +0500

    Added new function

--- helper_functions.py
+++ helper_functions.py
@@ -19,18 +19,23 @@ def calculation_in_seconds(timing):
 
 def sending_post(folder, image, bot, channel_id):
     path_image_post = os.path.join(folder, image)
-    try:
-        with open(path_image_post, 'rb') as picture_for_message:
-            bot.send_document(chat_id=channel_id, document=picture_for_message)
-    except telegram.error.NetworkError:
-        print('Соединение с интернетом разорвано, повторная отправка...')
-        time.sleep(10)
+    with open(path_image_post, 'rb') as picture_for_message:
+        bot.send_document(chat_id=channel_id, document=picture_for_message)
 
 
-def saving_image(folder, image, url, params={}):
+
+def saving_image(folder, image, url, params=None):
     image_path = os.path.join(folder, image)
     response = requests.get(url, params=params)
     response.raise_for_status()
 
     with open(image_path, 'wb') as file:
         file.write(response.content)
+
+
+def get_launches_data(images_id):
+    url = f'https://api.spacexdata.com/v5/launches/{images_id}'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    return response.json()
