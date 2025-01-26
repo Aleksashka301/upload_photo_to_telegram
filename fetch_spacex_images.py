1b0b82339c04cbcf1acb288e39dba417fc12200a
Aleksashka301 <aleksashka301@gmail.com>
2025-01-26 20:55:46 +0500

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,23 +1,31 @@
 from helper_functions import saving_image, get_launches_data
 import argparse
+import requests
 import os
 
 
-def get_spacex_images(images_id=None):
+def get_launch_images_by_id(images_id=None):
     if images_id:
-        launch = get_launches_data(images_id)
-        return launch['links']['flickr']['original']
+        try:
+            launch = get_launches_data(images_id)
+            return launch['links']['flickr']['original']
+        except requests.exceptions.HTTPError:
+            print(f'Запуска с таким id: {images_id} нет!')
+            return get_launch_images()
     else:
-        images_id = ''
-        launches = get_launches_data(images_id)
-
-        for launch in reversed(launches):
-            if launch['links']['flickr']['original']:
-                return launch['links']['flickr']['original']
+        return get_launch_images()
 
 
-def creation_spacex_images(images):
-    folder = os.path.join('photos from space', 'spacex')
+def get_launch_images():
+    images_id = ''
+    launches = get_launches_data(images_id)
+
+    for launch in reversed(launches):
+        if launch['links']['flickr']['original']:
+            return launch['links']['flickr']['original']
+
+
+def creation_spacex_images(images, folder):
     os.makedirs(folder, exist_ok=True)
 
     for image_number, url_image in enumerate(images):
@@ -26,12 +34,16 @@ def creation_spacex_images(images):
 
 
 if __name__ == '__main__':
+    folder = os.path.join('photos from space', 'spacex')
     parser = argparse.ArgumentParser()
+    parser.add_argument('directory', nargs='?', default=folder, type=str)
     parser.add_argument('images_id', nargs='?', default='')
     args = parser.parse_args()
+    directory = args.directory
     user_images_id = args.images_id
-    images = get_spacex_images(user_images_id)
 
-    creation_spacex_images(images)
-
-
+    try:
+        images = get_launch_images_by_id(user_images_id)
+        creation_spacex_images(images, directory)
+    except NotADirectoryError:
+        print('В данной системе нельзя использовать слов "con" или символы "\/:*?"<>|" для создания папок или файлов!')
